059a4d886b28a00337091bd6e8a4a40380ba8312
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-01-25 11:51:05 -0600

    testeando la aplicación

--- src/_V0/controllers/index.controllers.ts
+++ src/_V0/controllers/index.controllers.ts
@@ -1,9 +1,11 @@
 import { RequestHandler, ErrorRequestHandler } from "express"
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const pingPong : RequestHandler =(req, res, next)=>{
     try {
         // crear token y devolverlo
-        return res.status(200).json({message:"pong",data:null})
+        return res.status(200).json({message:"pong",data:process.env})
     } catch (error) {
         return res.status(500).json({message:"Error en el servidor", error})
     }
