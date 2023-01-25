01f1cabfdb40cf4b934cbeea261817833fd91d0a
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-01-25 12:01:45 -0600

    testeando la aplicación

--- src/_V0/controllers/index.controllers.ts
+++ src/_V0/controllers/index.controllers.ts
@@ -5,7 +5,14 @@ dotenv.config();
 export const pingPong : RequestHandler =(req, res, next)=>{
     try {
         // crear token y devolverlo
-        return res.status(200).json({message:"pong",data:process.env})
+        return res.status(200).json({message:"pong",data:{
+            secret:process.env.SECRET,
+            modo: process.env.NODE_ENV,
+            database: process.env.DATABASE,
+            user: process.env.USER,
+            host: process.env.HOST,
+            password: process.env.PASSWORD,
+        }})
     } catch (error) {
         return res.status(500).json({message:"Error en el servidor", error})
     }
