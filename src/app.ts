7a21ec1bbff28efbecfeb2d4ba1d15d4a9e229af
JosueGuzman <107532411+JosueElutiche24@users.noreply.github.com>
2023-01-25 20:19:08 -0600

    Se cambiaron los ajustes de los headers para tratar de solucionar errores

--- src/app.ts
+++ src/app.ts
@@ -13,11 +13,20 @@ app.set("port", port);
 
 // middlewares
 app.use(cookieParser());
-app.use(cors({
+  /*
+  app.use(cors({
      origin: ['http://localhost:3000',"http://localhost:5173"],
     //origin:'*', 
    credentials:true
   }));
+  */
+app.use((req, res, next)=>{
+  res.header("Access-Control-Allow-Origin","http://localhost:5173")
+  res.header("Access-Control-Allow-Credentials", "true")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Widht, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+  next();
+})
 app.use(express.json());
 
 // routes V0
